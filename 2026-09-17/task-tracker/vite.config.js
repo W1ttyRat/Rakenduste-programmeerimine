ae9fafc8501dae317b98d61b6b8586d62cf7bea0
Tanel Metshein <tanemets@tlu.ee>
2026-09-22 16:43:41 +0300

    fix: update base path in vite configuration

--- 2026-09-17/task-tracker/vite.config.js
+++ 2026-09-17/task-tracker/vite.config.js
@@ -2,10 +2,10 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Rakenduste-programmeerimine/2026-09-17/task-tracker/',
+  base: '/Rakenduste-programmeerimine/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: './src/testSetup.js',
   },
-});
+})
